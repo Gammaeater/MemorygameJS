85c7d3e2671fa729fc9654377b46cbeb606b2a3d
Livio <hipzap@gmail.com>
2019-04-10 12:50:10 +0200

    js adding function

---
+++ mgame.js
@@ -0,0 +1,36 @@
+const cardsColor=["red","red","green","green","blue","blue","brown","brown",
+"yellow","yellow","gray","gray","cadetblue","cadetblue","violet","violet",
+"lightgreen","lightgreen"];
+
+
+let cards = document.querySelectorAll('div');
+cards = [...cards]; //changing nodes to array
+
+// Function that random looing for random element ,add class to elemen + delete this color from tab
+
+
+const clickCard = function() {};
+
+const init = function() {
+    cards.forEach(function (card) {
+        const position = Math.floor(Math.random() *
+         cardsColor.length);
+         card.classList.add(cardsColor[position])
+         cardsColor.splice(position, 1);
+
+
+      
+    })
+
+
+    // function that add "hidden class"
+    setTimeout(function(){
+        cards.forEach(function(card) {
+           card.classList.add("hidden") 
+           card.addEventListener("click",clickCard)
+        })
+
+    }, 2000)
+
+}
+init()
